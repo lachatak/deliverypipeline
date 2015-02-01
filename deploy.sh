253f59555f4fdfe3bf2cff4e991faa90ac24649f
Krisztian Lachata <krisztian.lachata@gmail.com>
2015-02-01 21:24:57 +0000

    circle CI test

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ EB_BUCKET=elasticbeanstalk-eu-west-1-715628085737
 # Create new Elastic Beanstalk version
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+
+cat $DOCKERRUN_FILE
+
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name hello \
     --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
