44055595c9808ed102c1767387cc60b192de1dcd
Krisztian Lachata <krisztian.lachata@gmail.com>
2015-02-19 00:59:56 +0000

    add health check after deploy

--- release/release.rb
+++ release/release.rb
@@ -24,13 +24,28 @@ puts "Deploying version to environment #{target_environment_name}"
 %x[aws elasticbeanstalk create-application-version --application-name deliverypipeline --version-label #{app_version} --source-bundle S3Bucket=#{s3bucket},S3Key=#{app_version}.json --region #{region}]
 %x[aws elasticbeanstalk update-environment --environment-name #{target_environment_name} --version-label #{app_version} --region #{region}]
 
-results = JSON.parse(%x[aws elasticbeanstalk describe-environments --application-name #{application_name} --environment-names #{target_environment_name} --region #{region}])['Environments'][0]
+## Wait for the new environment to go ready
+## This wait can be customized for a given application to determine when it is really ready
 
-env_health = results['Health']
-env_status = results['Status']
-puts "Current Health: #{env_health}   Current Status: #{env_status}"
-if (("Green" != env_health) || ("Ready" != env_status))
-  abort "Environment must be Ready and Green. Current Health: #{env_health}  Current Status: #{env_status}"
+max_wait_time_sec = 5 * 60;  ## 5 minutes
+begin
+  Timeout::timeout(max_wait_time_sec) do
+    done = false
+    while (! done) do
+      puts "Checking if new environment is ready/green"
+      results = JSON.parse(%x[aws elasticbeanstalk describe-environments --application-name #{application_name} --environment-names #{target_environment_name} --region #{region}])['Environments'][0]
+
+      env_health = results['Health']
+      env_status = results['Status']
+      puts "Current Health: #{env_health} Current Status: #{env_status}"
+      done = "Ready" == env_status
+      if (! done)
+        sleep 20  ## seconds
+      end
+    end
+  end
+rescue Timeout::Error
+  abort "Environment does not seem to be launching in a reasonable time.  Exiting after #{max_wait_time_sec} seconds"
 end
 
 puts "Deployment was successful to #{application_name} -> #{target_environment_name}!"
